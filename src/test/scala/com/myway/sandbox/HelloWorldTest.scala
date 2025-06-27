fec40b17c63c1a8ad911ab855202b03de6812f4b
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2025-06-27 15:27:08 +0200

    added assertion

--- src/test/scala/com/myway/sandbox/HelloWorldTest.scala
+++ src/test/scala/com/myway/sandbox/HelloWorldTest.scala
@@ -1,11 +1,13 @@
 package com.myway.sandbox
 
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
-class HelloWorldTest extends AnyFunSuite {
+class HelloWorldTest extends AnyFunSuite with Matchers {
 
   test("greet returns correct greeting") {
     val result = HelloWorld.greet("Scala")
     assert(result == "Hello, Scala!")
+    HelloWorld.greet("Scala") shouldBe "Hello, Scala!"
   }
 }
