37ee971a1ad96e36d6685d02a4546c4a3ec506c6
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2025-06-28 10:23:52 +0200

    empty

---
+++ src/main/scala/com/myway/adventofcode/adv2024/Day9.scala
@@ -0,0 +1,17 @@
+package com.myway.adventofcode.adv2024
+
+import com.myway.adventofcode.tools.file.FileUtil
+
+object Day9 {
+
+  def main(args: Array[String]): Unit = {
+    val data: List[String] = FileUtil.readFile( "adventofcode/2024/day9.txt")
+    println(solve1(data))
+    println(solve2(data))
+  }
+
+  def solve1(data: List[String]): String =  ""
+
+  def solve2(data: List[String]): String =  ""
+
+}
