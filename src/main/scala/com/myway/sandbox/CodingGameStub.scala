7cd350cf753b892eb73c6205a041bcd821e441b3
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-05-27 00:11:01 +0200

    sample

---
+++ src/main/scala/com/myway/sandbox/CodingGameStub.scala
@@ -0,0 +1,24 @@
+package com.myway.sandbox
+
+import scala.io.StdIn._
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+object CodingGameStub extends App {
+
+  val readline = ???
+  def foo(): Unit = {
+
+
+  }
+
+  val size = readLine.toInt
+  val angle = readLine.toInt
+  val in: List[String] = List.range(0, size).foldLeft[List[String]](Nil)((acc, _) => acc ++ List(readLine))
+
+  in.map(println)
+  println("answer")
+
+}
