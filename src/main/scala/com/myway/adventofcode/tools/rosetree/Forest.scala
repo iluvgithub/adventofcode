f01975b4824ede8a38c29f361b6493a6e3ef9f44
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-06-07 00:59:39 +0200

    added zip

--- src/main/scala/com/myway/adventofcode/tools/rosetree/Forest.scala
+++ src/main/scala/com/myway/adventofcode/tools/rosetree/Forest.scala
@@ -14,7 +14,11 @@ case class Forest[A](roses: List[(A, Forest[A])]) {
       case Nil => out.head
       case ForestWrap(f) :: vs =>
         val heads: List[A] = f.roses.map(_._1)
-        go(phi, f.roses.map(_._2).map(ForestWrap(_)).reverse ++ (BranchStub(heads, heads.size) :: vs), out)
+        go(
+          phi,
+          f.roses.map(_._2).map(ForestWrap(_)).reverse ++ (BranchStub(heads, heads.size) :: vs),
+          out
+        )
 
       case BranchStub(as, n) :: vs =>
         val children  = as.zip(out.take(n))
@@ -26,10 +30,14 @@ case class Forest[A](roses: List[(A, Forest[A])]) {
   }
 
   def trace: String =
-    fold[String]( xs =>  s"${xs.map({case(u,v) =>
-      if(v.isEmpty) s"$u"
-        else
-      s"$u($v)"}).mkString(",")}"
+    fold[String](xs =>
+      s"${xs
+          .map { case (u, v) =>
+            if (v.isEmpty) s"$u"
+            else
+              s"$u($v)"
+          }
+          .mkString(",")}"
     )
 }
 
@@ -38,4 +46,50 @@ object Forest {
   def empty[A]: Forest[A]                     = Forest(Nil)
   def single[A](a: A): (A, Forest[A])         = (a, empty)
   def branch[A](forest: List[(A, Forest[A])]) = Forest(forest)
+
+  def initZipper[A]: ForestZipper[A] = ForestZipper(empty, Nil)
+}
+
+case class ForestContext[A](
+  lefts: List[(A, Forest[A])], // siblings before parent
+  parent: A,                   // parent label
+  rights: List[(A, Forest[A])] // siblings after parent
+)
+case class ForestZipper[A](focus: Forest[A], context: List[ForestContext[A]]) {
+
+  def down(i: Int): Option[ForestZipper[A]] = if (i >= focus.roses.size) None
+  else {
+    val left               = focus.roses.take(i)
+    val af: (A, Forest[A]) = focus.roses.drop(i).head
+    val right              = focus.roses.drop(i + 1)
+
+    Some(
+      ForestZipper(af._2, ForestContext(left, af._1, right) :: context)
+    )
+  }
+  def search(a: A): Option[Int] =
+    List.range(0, focus.roses.length).find(i => focus.roses(i)._1.equals(a))
+
+  def downBy(a: A): Option[ForestZipper[A]] = for {
+    i <- search(a)
+    z <- down(i)
+  } yield z
+
+  def up: Option[ForestZipper[A]] = context match {
+    case Nil => None
+    case x :: xs =>
+      Some(ForestZipper(Forest(x.lefts ++ ((x.parent, focus) :: x.rights)), xs))
+
+  }
+
+  @tailrec
+  final def upRoot: ForestZipper[A] = up match {
+    case None    => this
+    case Some(z) => z.upRoot
+  }
+
+  def prepend(a: A): ForestZipper[A] =
+    this.copy(
+      focus = Forest((a, Forest.empty[A]) :: focus.roses)
+    )
 }
--- src/test/scala/com/myway/adventofcode/tools/rosetree/ForestTest.scala
+++ src/test/scala/com/myway/adventofcode/tools/rosetree/ForestTest.scala
@@ -12,14 +12,29 @@ class ForestTest extends AnyFunSuite with Matchers {
       single('a') :: (
         'b',
         branch(
-          List(single('e'),
-            single('f')
-          ) ++ (single('d') :: Nil)
+          List(single('e'), single('f')) ++ (single('d') :: Nil)
         )
       ) :: Nil
     )
     // act && assert
     forest.trace shouldBe "a,b(e,f,d)"
+  }
+
+}
+
+class ForestZipperTest extends AnyFunSuite with Matchers {
+
+  test("build ") {
+    val zip0: ForestZipper[Char] = Forest.initZipper
+    val zip1 = zip0.prepend('d').prepend('c').prepend('b')
+    val optZip = zip1.downBy('c').map(_.prepend('f').prepend('e'))
+
+    optZip.isDefined shouldBe true
+    val zip = optZip.get
+
+    val f = zip.upRoot.focus
+
+    f.trace shouldBe "b,c(e,f),d"
   }
 
 }
