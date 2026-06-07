738f3497a26e342a35ee917f518628b1894152b3
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-06-07 15:24:36 +0200

    stack overflow secure implem

--- src/main/scala/com/myway/adventofcode/tools/rosetree/BinTree.scala
+++ src/main/scala/com/myway/adventofcode/tools/rosetree/BinTree.scala
@@ -2,8 +2,42 @@ package com.myway.adventofcode.tools.rosetree
 
 case class BinTree[A](left: Option[BinTree[A]], a: A, right: Option[BinTree[A]]) {
 
-  def fold[B](f: Option[B] => A => Option[B] => B): B =
-    f(left.map(_.fold(f)))(a)(right.map(_.fold(f)))
+  def fold[B](f: Option[B] => A => Option[B] => B): B = {
+
+    sealed trait Frame
+    case class BranchStub(a: A, hasLeft: Boolean, hasRight: Boolean) extends Frame
+    case class Wrap(node: BinTree[A])                                extends Frame
+
+    @annotation.tailrec
+    def loop(stack: List[Frame], results: List[B]): B =
+      stack match {
+        case Nil => results.head
+        case Wrap(nd) :: rest =>
+          val (wraps, stub) =
+            if (nd.left.isDefined && nd.right.isDefined)
+              (Wrap(nd.left.get) :: Wrap(nd.right.get) :: Nil, BranchStub(nd.a, true, true))
+            else if (nd.left.isDefined)
+              (Wrap(nd.left.get) :: Nil, BranchStub(nd.a, true, false))
+            else if (nd.right.isDefined)
+              (Wrap(nd.right.get) :: Nil, BranchStub(nd.a, false, true))
+            else (Nil, BranchStub(nd.a, false, false))
+
+          val newStack = wraps ++ (stub :: rest)
+          loop(newStack, results)
+
+        case BranchStub(a, hasLeft, hasRight) :: rest =>
+          val (result, dropSize) =
+            if (hasLeft && hasRight) (f(Some(results.tail.head))(a)(Some(results.head)), 2)
+            else if (hasLeft) (f(Some(results.head))(a)(None), 1)
+            else if (hasRight) (f(None)(a)(Some(results.head)), 1)
+            else (f(None)(a)(None), 0)
+
+          loop(rest, result :: results.drop(dropSize))
+
+      }
+
+    loop(List(Wrap(this)), Nil)
+  }
 
   def trace: String = fold[String](ol =>
     s =>
@@ -51,9 +85,9 @@ case class BinTreeZipper[A](focus: BinTree[A], contexts: List[BinTreeContext[A]]
       )
   }
 
-  def upRoot:BinTreeZipper[A] = up match {
-    case None =>this
-    case Some(z)=>z.upRoot
+  def upRoot: BinTreeZipper[A] = up match {
+    case None    => this
+    case Some(z) => z.upRoot
   }
 
   def setOnFocus(newA: A): BinTreeZipper[A] = this.copy(focus = focus.copy(a = newA))
