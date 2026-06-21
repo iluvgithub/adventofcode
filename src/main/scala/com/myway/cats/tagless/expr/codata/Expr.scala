2edc7baff1f3a107bca236b69bc1d879524a5d9c
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-06-21 17:41:11 +0200

    codata

---
+++ src/main/scala/com/myway/cats/tagless/expr/codata/Expr.scala
@@ -0,0 +1,35 @@
+package com.myway.cats.tagless.expr.codata
+
+trait Expr { self =>
+  def eval: Double
+  def print: String
+
+  def add(that:Expr):Expr = new Expr {
+    def eval: Double = self.eval + that.eval
+    def print: String = s"(${self.print} + ${that.print})"
+  }
+  def sub(that:Expr):Expr = new Expr {
+    def eval: Double = self.eval - that.eval
+    def print: String = s"(${self.print} - ${that.print})"
+  }
+
+  def mult(that:Expr):Expr = new Expr {
+    def eval: Double = self.eval * that.eval
+    def print: String = s"(${self.print} * ${that.print})"
+  }
+  def div(that:Expr):Expr = new Expr {
+    def eval: Double = self.eval / that.eval
+    def print: String = s"(${self.print} / ${that.print})"
+  }
+}
+object Expr {
+
+  def literal(value: Double): Expr =
+    new Expr {
+      def eval: Double = value
+      def print: String = value.toString
+    }
+
+  val ZERO: Expr = literal(0)
+
+}
---
+++ src/test/scala/com/myway/cats/tagless/expr/codata/EvalInterpreterTest.scala
@@ -0,0 +1,30 @@
+package com.myway.cats.tagless.expr.codata
+
+import com.myway.cats.tagless.expr.data.DoubleInterpreter
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+class EvalInterpreterTest extends AnyFunSuite with Matchers {
+
+  import Expr._
+
+  val onePlusTwo: Expr = literal(1) add literal(2)
+  val onePlusTwoTimesThree: Expr = onePlusTwo mult literal(3)
+  val oneMinusTwo: Expr = literal(1) sub literal(2)
+  val oneDivTwo: Expr = literal(1) div literal(2)
+
+  test("eval double") {
+    onePlusTwo.eval shouldBe 3
+    onePlusTwoTimesThree.eval shouldBe 9
+    oneMinusTwo.eval shouldBe -1
+    oneDivTwo.eval shouldBe 0.5
+  }
+
+
+  test("print") {
+    onePlusTwo.print shouldBe "(1.0 + 2.0)"
+    onePlusTwoTimesThree.print  shouldBe "((1.0 + 2.0) * 3.0)"
+    oneMinusTwo.print shouldBe "(1.0 - 2.0)"
+    oneDivTwo.print shouldBe "(1.0 / 2.0)"
+  }
+}
