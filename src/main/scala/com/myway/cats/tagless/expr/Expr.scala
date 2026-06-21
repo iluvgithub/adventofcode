07ef2f98a266104b4ae6d62717a8c24b4940ea8d
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-06-21 17:26:40 +0200

    first interpreter

---
+++ src/main/scala/com/myway/cats/tagless/expr/Expr.scala
@@ -0,0 +1,15 @@
+package com.myway.cats.tagless.expr
+
+trait Expr {
+
+}
+
+case class Add(l: Expr, r: Expr) extends Expr
+
+case class Sub(l: Expr, r: Expr) extends Expr
+
+case class Mul(l: Expr, r: Expr) extends Expr
+
+case class Div(l: Expr, r: Expr) extends Expr
+
+case class Literal(value: Double) extends Expr
