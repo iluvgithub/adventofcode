9437fea7b972c853b37d67f56553bb8005897a8f
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-06-21 17:42:32 +0200

    codata

--- src/main/scala/com/myway/cats/tagless/expr/codata/Expr.scala
+++ src/main/scala/com/myway/cats/tagless/expr/codata/Expr.scala
@@ -1,32 +1,42 @@
 package com.myway.cats.tagless.expr.codata
 
-trait Expr { self =>
+trait Expr {
+  self =>
   def eval: Double
+
   def print: String
 
-  def add(that:Expr):Expr = new Expr {
+  def add(that: Expr): Expr = new Expr {
     def eval: Double = self.eval + that.eval
+
     def print: String = s"(${self.print} + ${that.print})"
   }
-  def sub(that:Expr):Expr = new Expr {
+
+  def sub(that: Expr): Expr = new Expr {
     def eval: Double = self.eval - that.eval
+
     def print: String = s"(${self.print} - ${that.print})"
   }
 
-  def mult(that:Expr):Expr = new Expr {
+  def mult(that: Expr): Expr = new Expr {
     def eval: Double = self.eval * that.eval
+
     def print: String = s"(${self.print} * ${that.print})"
   }
-  def div(that:Expr):Expr = new Expr {
+
+  def div(that: Expr): Expr = new Expr {
     def eval: Double = self.eval / that.eval
+
     def print: String = s"(${self.print} / ${that.print})"
   }
 }
+
 object Expr {
 
   def literal(value: Double): Expr =
     new Expr {
       def eval: Double = value
+
       def print: String = value.toString
     }
 
