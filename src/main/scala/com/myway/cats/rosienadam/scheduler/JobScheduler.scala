60f9844a8f8af2a107540caeac871e44d19dd03e
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-06-28 16:52:37 +0200

    added method to handle state

--- src/main/scala/com/myway/cats/rosienadam/scheduler/JobScheduler.scala
+++ src/main/scala/com/myway/cats/rosienadam/scheduler/JobScheduler.scala
@@ -1,7 +1,7 @@
 package com.myway.cats.rosienadam.scheduler
 
 import cats.data.Chain
-import cats.effect.{IO, Ref, Resource}
+import cats.effect.{IO, Ref}
 import com.myway.cats.rosienadam.core._
 
 trait JobScheduler[A] {
@@ -10,21 +10,34 @@ trait JobScheduler[A] {
 
 object JobScheduler {
   case class JobSchedulerMemoryState[A](
-                       maxRunning: Int,
-                       scheduled: Chain[Scheduled[A]] = Chain.empty[Scheduled[A]],
-                       running: Map[JobId, Running[A]] = Map.empty[JobId,Running[A]],
-                       completed: Chain[Completed[A]] = Chain.empty[Completed[A]]
+    maxRunning: Int,
+    scheduled: Chain[Scheduled[A]] = Chain.empty[Scheduled[A]],
+    running: Map[JobId, Running[A]] = Map.empty[JobId, Running[A]],
+    completed: Chain[Completed[A]] = Chain.empty[Completed[A]]
   ) {
 
-    def enqueue(job: Scheduled[A]): JobSchedulerMemoryState[A] = copy(scheduled = scheduled :+ job)
+    def enqueue(job: Scheduled[A]): JobSchedulerMemoryState[A] =
+      copy(scheduled = scheduled :+ job)
 
+    def dequeue: (JobSchedulerMemoryState[A], Option[Scheduled[A]]) =
+      scheduled.uncons
+        .filter(_ => running.size < maxRunning)
+        .map { case (head, tail) =>
+          copy(scheduled = tail) -> Some(head)
+        }
+        .getOrElse(this -> None)
+    def appendRunning(job: Running[A]): JobSchedulerMemoryState[A] =
+      copy(running = running + (job.jobId -> job))
+
+    def onComplete(job: Completed[A]): JobSchedulerMemoryState[A] =
+      copy(completed = completed :+ job, running = running - job.jobId)
   }
 
-  def schedulerMem[A](ref: Ref[IO, JobSchedulerMemoryState[A]]):JobScheduler[A] =
-    (task: IO[A]) => for {
-      job <- JobSyntax.create(task)
-      _ <- ref.update(_.enqueue(job))
-    } yield job.jobId
-
+  def schedulerMem[A](ref: Ref[IO, JobSchedulerMemoryState[A]]): JobScheduler[A] =
+    (task: IO[A]) =>
+      for {
+        job <- JobSyntax.create(task)
+        _   <- ref.update(_.enqueue(job))
+      } yield job.jobId
 
 }
