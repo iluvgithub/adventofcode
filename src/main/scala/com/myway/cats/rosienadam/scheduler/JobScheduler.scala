5076563645fe034fc8c6ecd669365edf469aebcd
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-06-28 22:00:54 +0200

    JobSchedulerWithBinaryMachine

--- src/main/scala/com/myway/cats/rosienadam/scheduler/JobScheduler.scala
+++ src/main/scala/com/myway/cats/rosienadam/scheduler/JobScheduler.scala
@@ -1,8 +1,10 @@
 package com.myway.cats.rosienadam.scheduler
 
 import cats.data.Chain
-import cats.effect.{IO, Ref}
+import cats.effect.{IO, Outcome, Ref, Resource}
+import cats.implicits.toFunctorOps
 import com.myway.cats.rosienadam.core._
+import com.myway.cats.rosienadam.reactor.Reactor
 
 trait JobScheduler[A] {
   def schedule(task: IO[A]): IO[JobId]
@@ -50,4 +52,26 @@ object JobScheduler {
         _     <- zzz.wakeUp
       } yield jobId
 
+}
+
+object JobSchedulerWithBinaryMachine {
+
+  import com.myway.cats.rosienadam.scheduler.JobScheduler.JobSchedulerMemoryState
+  def resource[A](maxRunning: Int): IO[Resource[IO, JobScheduler[A]]] =
+    for {
+      stateRef       <- IO(JobSchedulerMemoryState[A](maxRunning))
+      schedulerState <- Ref[IO].of(stateRef)
+      machine        <- BinaryStateMachine.asleep
+      scheduler = JobScheduler.schedulerMemSleep(schedulerState, machine)
+      reactor   = Reactor.fromStateRef[A](schedulerState)
+      onStart0  = (id: JobId) => IO.unit
+      onComplete0 = (id: JobId, outcome: Outcome[IO, Throwable, A]) =>
+        outcome.fold[IO[Either[String, A]]](
+          IO(Left(s"Cancelled job id:$id")),
+          e => IO(Left(e.getMessage)),
+          _.map(Right(_))
+        )
+      loop = Reactor.loop(reactor, machine, onStart0, onComplete0)
+
+    } yield loop.background.as(scheduler)
 }
