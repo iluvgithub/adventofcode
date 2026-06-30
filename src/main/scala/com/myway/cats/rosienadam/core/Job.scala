90d26fb1cdb5706ca93f03dd5fefa54ad34918e0
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-06-30 23:18:37 +0200

    avoid confusion of type Scheduled/Running/Completed

--- src/main/scala/com/myway/cats/rosienadam/core/Job.scala
+++ src/main/scala/com/myway/cats/rosienadam/core/Job.scala
@@ -6,40 +6,42 @@ import cats.effect.{Fiber, IO}
 
 import java.util.UUID
 
-trait Job
-
-case class Scheduled[A](jobId: JobId, task: IO[A]) extends Job {
-  def start: IO[Running[A]] =
-    for {
-      exitCase <- Deferred[IO, Outcome[IO, Throwable, A]]
-      fiber <- task
-        .guaranteeCase(x =>
-          exitCase
-            .complete(x match {
-              case Succeeded(fa) => Outcome.succeeded(fa)
-              case Errored(t)    => Outcome.errored(t)
-              case Canceled()    => Outcome.canceled
-            })
-            .void
-        )
-        .start
-    } yield Running(jobId, fiber, exitCase)
-}
-
-case class Running[A](
-  jobId: JobId,
-  fiber: Fiber[IO, Throwable, A],
-  exitCase: Deferred[IO, Outcome[IO, Throwable, A]]
-) extends Job {
-  val await: IO[Completed[A]] =
-    exitCase.get.map(Completed(jobId, _))
-
-  def cancel : IO[Unit] = fiber.cancel
-}
-
-case class Completed[A](jobId: JobId, exitCase: Outcome[IO, Throwable, A]) extends Job
+trait Job[A]
 
 object JobSyntax {
+
+
+  case class Scheduled[A](jobId: JobId, task: IO[A]) extends Job[A] {
+    def start: IO[Running[A]] =
+      for {
+        exitCase <- Deferred[IO, Outcome[IO, Throwable, A]]
+        fiber <- task
+          .guaranteeCase(x =>
+            exitCase
+              .complete(x match {
+                case Succeeded(fa) => Outcome.succeeded(fa)
+                case Errored(t)    => Outcome.errored(t)
+                case Canceled()    => Outcome.canceled
+              })
+              .void
+          )
+          .start
+      } yield Running(jobId, fiber, exitCase)
+  }
+
+  case class Running[A](
+                         jobId: JobId,
+                         fiber: Fiber[IO, Throwable, A],
+                         exitCase: Deferred[IO, Outcome[IO, Throwable, A]]
+                       ) extends Job[A] {
+    val await: IO[Completed[A]] =
+      exitCase.get.map(Completed(jobId, _))
+
+    def cancel : IO[Unit] = fiber.cancel
+  }
+
+  case class Completed[A](jobId: JobId, exitCase: Outcome[IO, Throwable, A]) extends Job[A]
+
   def create[A](task: IO[A]): IO[Scheduled[A]] =
     IO(JobId(UUID.randomUUID().toString)).map(Scheduled(_, task))
 }
