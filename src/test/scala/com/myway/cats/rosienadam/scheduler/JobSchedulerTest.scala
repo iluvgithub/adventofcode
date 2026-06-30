0fda5dad766a7096af8ad85bb020be255621b5bd
Pascal BOSSAVIT <pascal.bossavit@gmail.com>
2026-06-30 23:22:01 +0200

    make state trait

--- src/test/scala/com/myway/cats/rosienadam/scheduler/JobSchedulerTest.scala
+++ src/test/scala/com/myway/cats/rosienadam/scheduler/JobSchedulerTest.scala
@@ -5,7 +5,6 @@ import cats.effect.kernel.Outcome
 import cats.effect.testkit.TestControl
 import cats.effect.{IO, Ref}
 import com.myway.cats.rosienadam.core.JobSyntax.Scheduled
-import com.myway.cats.rosienadam.core.Scheduled
 import munit.CatsEffectSuite
 
 class JobSchedulerTest extends CatsEffectSuite {
@@ -15,12 +14,12 @@ class JobSchedulerTest extends CatsEffectSuite {
     val maxRunning                       = 2
     val task                             = IO("ABC")
     val state: JobSchedulerState[String] = JobScheduler.JobSchedulerMemoryState[String](maxRunning)
-    val refIo: IO[Ref[IO, JobSchedulerState[String]]] =Ref[IO].of(state)
+    val refIo: IO[Ref[IO, JobSchedulerState[String]]] = Ref[IO].of(state)
     val actualIO: IO[String] = for {
       ref <- refIo
       scheduler = JobScheduler.makeScheduler(ref)
       jobId     <- scheduler.schedule(task)
-      scheduled  <- ref.get.map[Scheduled[String]](_.findFirstScheduled.get)
+      scheduled <- ref.get.map[Scheduled[String]](_.findFirstScheduled.get)
       running   <- scheduled.start
       o         <- running.await.flatMap(_.exitCase.fold(IO("cancel"), _ => IO(""), identity))
     } yield o
